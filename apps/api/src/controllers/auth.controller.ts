1f579dc60c26635d330c13e86950e281bf7b4a45
devadarshh <imadarshsingh2002@gmail.com>
2025-08-26 10:19:18 +0530

    added user.updated and user.delted event in auth controller

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,10 +1,9 @@
 import { verifyWebhook } from "@clerk/express/webhooks";
 import { Request, Response } from "express";
 import prisma from "../prisma";
-
 import { z } from "zod";
 
-const userCreatedSchema = z.object({
+const userSchema = z.object({
   id: z.string(),
   first_name: z.string().optional(),
   last_name: z.string().optional(),
@@ -24,13 +23,13 @@ export const syncUserInDB = async (
 ): Promise<Response> => {
   try {
     const evt = await verifyWebhook(req, {
-      signingSecret: process.env.CLERK_WEBHOOK_SECRET_USER,
+      signingSecret: process.env.CLERK_WEBHOOK_SECRET_USER!,
     });
 
     const eventType = evt.type;
 
     if (eventType === "user.created") {
-      const parseResult = userCreatedSchema.safeParse(evt.data);
+      const parseResult = userSchema.safeParse(evt.data);
       if (!parseResult.success) {
         return res.status(400).json({
           success: false,
@@ -40,28 +39,59 @@ export const syncUserInDB = async (
       }
 
       const data = parseResult.data;
-      const clerkId = data.id;
-      const firstName = data.first_name || null;
-      const lastName = data.last_name || null;
-
-      const email: string =
-        data.email_addresses?.[0]?.email_address.toLowerCase() || "";
-
-      const imageUrl = data.image_url || null;
-
       await prisma.user.create({
         data: {
-          clerkId,
-          firstName,
-          lastName,
-          email,
-          imageUrl,
+          clerkId: data.id,
+          firstName: data.first_name || null,
+          lastName: data.last_name || null,
+          email: data.email_addresses?.[0]?.email_address.toLowerCase() || "",
+          imageUrl: data.image_url || null,
         },
       });
 
       return res.status(201).json({
         success: true,
         message: "User created successfully",
+      });
+    }
+
+    if (eventType === "user.updated") {
+      const parseResult = userSchema.safeParse(evt.data);
+      if (!parseResult.success) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid user data",
+          errors: parseResult.error,
+        });
+      }
+
+      const data = parseResult.data;
+      await prisma.user.update({
+        where: { clerkId: data.id },
+        data: {
+          firstName: data.first_name || null,
+          lastName: data.last_name || null,
+          email: data.email_addresses?.[0]?.email_address.toLowerCase() || "",
+          imageUrl: data.image_url || null,
+        },
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      const clerkId = (evt.data as any).id;
+
+      await prisma.user.delete({
+        where: { clerkId },
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
       });
     }
 
