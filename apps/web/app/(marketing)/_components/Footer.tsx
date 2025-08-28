fdc635873c2f0692270283fce8e5e1a58c43a351
devadarshh <imadarshsingh2002@gmail.com>
2025-08-28 20:25:04 +0530

    fix typescript error in org folder and root

--- apps/web/app/(marketing)/_components/Footer.tsx
+++ apps/web/app/(marketing)/_components/Footer.tsx
@@ -6,12 +6,10 @@ export const Footer = () => {
   return (
     <footer className="w-full border-t bg-slate-100 p-4">
       <div className="mx-auto grid grid-cols-1 md:grid-cols-3 items-center max-w-screen-2xl text-center md:text-left gap-4">
-        {/* Logo (Left) */}
         <div className="flex justify-center md:justify-start">
           <Logo aria-label="Company Logo" />
         </div>
 
-        {/* Credit (Middle) */}
         <div className="flex justify-center">
           <p className="flex items-center gap-1 text-sm text-slate-600">
             Made with <Heart className="w-3 h-3 text-red-500" /> by{" "}
@@ -26,7 +24,6 @@ export const Footer = () => {
           </p>
         </div>
 
-        {/* Links (Right) */}
         <nav className="flex justify-center md:justify-end space-x-4">
           <Button
             size="sm"
