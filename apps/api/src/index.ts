8ccd057782fea668795ac4643c763e1d494d1313
devadarshh <imadarshsingh2002@gmail.com>
2025-08-30 08:52:06 +0530

    fix: cors error

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://trelloid-lovat.vercel.app",
+  "https://trelloidapp.vercel.app",
 ];
 
 app.use(
